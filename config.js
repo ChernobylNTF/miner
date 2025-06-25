7c932879695caf6df33a26af8c4079f4dd1025e0
ChernobylNTF <wallydd1983@gmail.com>
2025-06-25 11:22:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Este es el contenido de tu archivo config.js
 const CONTRACT_ADDRESS = "0x55E6C9C22C0eaD68F0be7CdcB5d8BAa636a8A1a0";
 const WORLDCOIN_APP_ID = "app_158d2ce5b87e96111765af5b41168b93";
+const ACTION_ID = "test-action"
